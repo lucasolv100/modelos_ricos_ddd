48f63df1edb3e4e9638c2af8b81e6dfc9831288c
Lucas Souza <lucas.souza@devplace.com.br>
2019-05-23 09:56:48 -0300

    Criado estrutura do dominio; Adicionado projeto de testes; Adicionado referências em classlibs;

---
+++ PaymentContext/PaymentsContext.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PaymentsContext.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ PaymentContext/PaymentsContext.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PaymentsContext.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

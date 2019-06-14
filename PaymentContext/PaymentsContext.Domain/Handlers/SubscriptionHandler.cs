937cfb1de89f3462cb358fb40b54c195fdc5da60
Lucas Souza <lucas.souza@devplace.com.br>
2019-06-14 09:15:20 -0300

    iniciado command handlers

--- PaymentContext/PaymentsContext.Domain/Handlers/SubscriptionHandler.cs
+++ PaymentContext/PaymentsContext.Domain/Handlers/SubscriptionHandler.cs
@@ -1,5 +1,6 @@
 using Flunt.Notifications;
 using PaymentsContext.Domain.Commands;
+using PaymentsContext.Domain.Repositories;
 using PaymentsContext.Share.Commands;
 using PaymentsContext.Share.Handlers;
 
@@ -7,11 +8,32 @@ namespace PaymentsContext.Domain.Handlers
 {
     public class SubscriptionHandler : Notifiable, IHandler<CreateBoletoSubscriptionCommand>
     {
+        private readonly IStudentRepository _repository;
+
+        public SubscriptionHandler(
+            IStudentRepository repository
+        )
+        {
+            _repository = repository;
+        }
         public ICommandResult Handle(CreateBoletoSubscriptionCommand command)
         {
+            //Fail fast validation
+            command.Validate();
+            if (command.Invalid)
+            {
+                AddNotifications(command);
+                return new CommandResult(false, "Não foi possivel realizar o cadastro");
+            }
+                
+
             //Verificar se o documento já existe;
-            
+            if(_repository.DocumentExists(command.Document))
+                AddNotification("Document", "Este CPF já está em uso");
+
             //Verificar se o email já existe;
+            if(_repository.EmailExists(command.Email))
+                AddNotification("Document", "Este e-mail já está em uso");
 
             //Gerar os VOs
 
@@ -23,6 +45,10 @@ namespace PaymentsContext.Domain.Handlers
 
             //Enviar email de boas vindas;
 
+            return new CommandResult(true, "Assinatura realizada com sucesso!");
+
         }
+
+
     }
 }

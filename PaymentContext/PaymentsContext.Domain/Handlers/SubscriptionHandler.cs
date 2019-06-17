5dfae005461740287d01d27a77debef07d8b317d
Lucas Souza <lucas.souza@devplace.com.br>
2019-06-17 08:59:59 -0300

    Aplicado handler

--- PaymentContext/PaymentsContext.Domain/Handlers/SubscriptionHandler.cs
+++ PaymentContext/PaymentsContext.Domain/Handlers/SubscriptionHandler.cs
@@ -1,6 +1,11 @@
+using System;
 using Flunt.Notifications;
 using PaymentsContext.Domain.Commands;
+using PaymentsContext.Domain.Entities;
+using PaymentsContext.Domain.Enums;
 using PaymentsContext.Domain.Repositories;
+using PaymentsContext.Domain.Services;
+using PaymentsContext.Domain.ValueObjects;
 using PaymentsContext.Share.Commands;
 using PaymentsContext.Share.Handlers;
 
@@ -9,12 +14,15 @@ namespace PaymentsContext.Domain.Handlers
     public class SubscriptionHandler : Notifiable, IHandler<CreateBoletoSubscriptionCommand>
     {
         private readonly IStudentRepository _repository;
+        private readonly IEmailService _emailService;
 
         public SubscriptionHandler(
-            IStudentRepository repository
+            IStudentRepository repository,
+            IEmailService emailService
         )
         {
             _repository = repository;
+            _emailService = emailService;
         }
         public ICommandResult Handle(CreateBoletoSubscriptionCommand command)
         {
@@ -25,25 +33,50 @@ namespace PaymentsContext.Domain.Handlers
                 AddNotifications(command);
                 return new CommandResult(false, "Não foi possivel realizar o cadastro");
             }
-                
+
 
             //Verificar se o documento já existe;
-            if(_repository.DocumentExists(command.Document))
+            if (_repository.DocumentExists(command.Document))
                 AddNotification("Document", "Este CPF já está em uso");
 
             //Verificar se o email já existe;
-            if(_repository.EmailExists(command.Email))
+            if (_repository.EmailExists(command.Email))
                 AddNotification("Document", "Este e-mail já está em uso");
 
             //Gerar os VOs
-
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document, EDocumentType.CPF);
+            var email = new Email(command.Email);
+            var address = new Address(command.Street, command.Number, command.Neighborhood, command.City, command.State, command.ZipCode);
+            
             //Gerar as entidades
+            var student = new Student(name, document, email);
+            var subscription = new Subscription(DateTime.Now.AddMonths(1));
+            var payment = new BoletoPayment(
+                command.BarCode,
+                new Email(command.PayerEmail),
+                command.OurNumber,
+                command.PaidDate,
+                command.ExpireDate,
+                command.Total,
+                command.TotalPaid,
+                address,
+                new Document(command.PayerDocument, command.PayerDocumentType),
+                command.FirstName
+            );
+
+            //Relacionamentos
+            subscription.AddPayment(payment);
+            student.AddSubscription(subscription);
 
             //Aplicar as validações;
+            AddNotifications(name, email, document, address, payment, student, subscription);
 
             //Salvar as informações;
+            _repository.CreateSubscription(student);
 
             //Enviar email de boas vindas;
+            _emailService.Send(name.ToString(), command.Email, "bem vindo ao curso xpto", "sua assinatura foi concluida com sucesso");
 
             return new CommandResult(true, "Assinatura realizada com sucesso!");
 
--- PaymentContext/PaymentsContext.Domain/ValueObjects/Name.cs
+++ PaymentContext/PaymentsContext.Domain/ValueObjects/Name.cs
@@ -25,5 +25,9 @@ namespace PaymentsContext.Domain.ValueObjects
         }
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
+
+        public override string ToString(){
+            return $"{FirstName} {LastName}";
+        }
     }
 }
